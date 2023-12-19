ae37403cb727b70d93da331ab66bf79765bf5ec2
WendyLuna1996 <113041956+WendyLuna1996@users.noreply.github.com>
2023-12-19 18:28:11 -0500

    Add files via upload

---
+++ Clientes.py
@@ -0,0 +1,67 @@
+from Conexion import *
+
+class CClientes:
+
+    def mostrarClientes():
+      try:
+         cone = CConexion.ConexionBaseDeDatos()
+         cursor = cone.cursor()
+         cursor.execute("select * from usuarios;")
+         miResultado = cursor.fetchall()
+         cone.commit()
+         cone.close()
+         return miResultado
+        
+      except mysql.connector.Error as error:
+           print("Error de mostrar datos{}".format(error))
+          
+
+    def ingresarClientes(nombres,apellidos,sexo):
+
+        try:
+         cone = CConexion.ConexionBaseDeDatos()
+         cursor = cone.cursor()
+         sql ="insert into usuarios values(null,%s,%s,%s);"
+         #La variable valores tiene que ser una tupla
+         #Como minima expresion es: (valor,) la coma hace que sea una tupla
+         #Las tuplas son listas inmutables, eso quiere decir que no se pueden modificar
+         valores = (nombres,apellidos,sexo)
+         cursor.execute(sql,valores)
+         cone.commit()
+         print(cursor.rowcount,"Registro ingresado")
+         cone.close()
+        
+
+        except mysql.connector.Error as error:
+            print("Error de ingreso de datos{}".format(error))
+
+    def modificarClientes(idUsuario,nombres,apellidos,sexo):
+
+     try:
+          cone = CConexion.ConexionBaseDeDatos()
+          cursor = cone.cursor()
+          sql ="UPDATE usuarios SET usuarios.nombres = %s,usuarios.apellidos = %s,usuarios.sexo = %sWhere usuarios. id =%s;"
+          valores = (nombres,apellidos,sexo,idUsuario)
+          cursor.execute(sql,valores)
+          print(cursor.rowcount,"Registro Actualizado")
+          cone.close()
+
+     except mysql.connector.Error as error:
+       print("Error de la actualizacion de datos {}".format(error))
+
+    def EliminarClientes(idUsuario):
+   
+       try:
+         cone = CConexion.ConexionBaseDeDatos()
+         cursor = cone.cursor()
+         sql ="DELETE from usuarios WHERE usuarios.id= %s;"
+         valores = (idUsuario,)
+         cursor.execute(sql,valores)
+         cone.commit()
+
+         print(cursor.rowcount,"Registro Eliminado")
+         cone.close()
+      
+       except mysql.connector.Error as error:
+             print("Error de eliminacion de datos {}".format(error))   
+             
---
+++ Conexion.py
@@ -0,0 +1,21 @@
+# pip install mysql-connector-python
+import mysql.connector
+
+class CConexion:
+
+    def ConexionBaseDeDatos():
+      try:
+         conexion = mysql.connector.connect(user='root',password='0000',
+                                               host='127.0.0.1',
+                                               database='clientesdb',
+                                               port='3308')
+         print("conexion correcta")
+
+         return conexion
+
+      except mysql.connector.Error as error:
+        print("Error al conectarte a la base de Datos {}".format(error))
+
+        return conexion 
+
+    ConexionBaseDeDatos()
